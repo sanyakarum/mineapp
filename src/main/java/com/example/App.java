5fcd7eadd3d7a763073a4bd3aeea3ebc2bdc0758
sanyakarum <karumsanya@gmail.com>
2025-06-09 10:09:09 +0000

    first commit=mineapp

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+         System.out.println( "good day!" );
+         int a=6;
+         int b=1;
+          System.out.println( "the sum is "+sum(a,b) );
+    }
+    public static int sum(int x, int y)
+    {
+        return x+y;
+    }
+}
